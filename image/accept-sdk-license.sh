a259aa7fef4849f70970a8e9fa82f8c467747e9d
Ovidiu-Florin BOGDAN <ovidiu.b13@gmail.com>
2015-10-10 03:01:01 +0300

    Moved and addapted init script in Dockerfile.

---
+++ image/accept-sdk-license.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/expect -f
+
+set timeout 1800
+
+# spawn update command and let's wait for password question
+spawn $env(ADIR)/android-sdk-linux/tools/android update sdk --no-ui --filter tools,platform-tools,build-tools-23.0.1,android-22
+expect {
+  "Do you accept the license '*'*" {
+        exp_send "y\r"
+        exp_continue
+  }
+  eof
+}
