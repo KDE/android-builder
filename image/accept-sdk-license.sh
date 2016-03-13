cf4ea71900610a02dc2d6a0a51750a77b6203a8f
Andreas Cord-Landwehr <cordlandwehr@kde.org>
2016-03-13 09:35:43 +0100

    Use "--all" to be able to select any ABI.
    Without this parameter, only the latest ABI is visible, which is
    much harder to maintain since the SDK regularly updates.

--- image/accept-sdk-license.sh
+++ image/accept-sdk-license.sh
@@ -3,7 +3,7 @@
 set timeout 1800
 
 # spawn update command and let's wait for password question
-spawn $env(ADIR)/android-sdk-linux/tools/android update sdk --no-ui --filter tools,platform-tools,build-tools-23.0.1,android-22,sys-img-armeabi-v7a-android-22
+spawn $env(ADIR)/android-sdk-linux/tools/android update sdk --no-ui --all --filter tools,platform-tools,build-tools-23.0.1,android-22,sys-img-armeabi-v7a-android-22
 expect {
   "Do you accept the license '*'*" {
         exp_send "y\r"
